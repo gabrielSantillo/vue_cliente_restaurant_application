768f262f1e1b0d3813e2910099848b947b211229
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2022-08-24 12:50:43 -0600

    Starting to develop the delete feature

--- src/components/delete-profile/DeleteProfile.vue
+++ src/components/delete-profile/DeleteProfile.vue
@@ -1,12 +1,23 @@
 <template>
     <div>
-        <button>Delete</button>
+         <div class="delete_div">
+        <h1>Delete your profile</h1>
+        <input type="text" placeholder="password" ref="pasword">
+        <button @click="delete_profile">Delete</button>
+    </div>
+    <div v-if="log_in_token === null">
+      <h2>You must log in first.</h2>
+      <router-link to="/login">Log In</router-link>
+    </div>
     </div>
 </template>
 
 <script>
+import cookies from "vue-cookies"
     export default {
-        
+          mounted() {
+                this.log_in_token = cookies.get(`log_in_token`);
+  },
     }
 </script>
 
--- src/views/DeletePage.vue
+++ src/views/DeletePage.vue
@@ -1,32 +1,21 @@
 <template>
   <div>
-    <div class="delete_div">
-      <h1>Delete Profile</h1>
-    </div>
-    <div v-if="log_in_token === null">
-      <h2>You must log in first.</h2>
-      <router-link to="/login">Log In</router-link>
-    </div>
+    <delete-profile></delete-profile>
   </div>
 </template>
 
 <script>
-import cookies from 'vue-cookies'
-export default {
-      data() {
-    return {
-      log_in_token: null,
-    };
-  },
+import DeleteProfile from '@/components/delete-profile/DeleteProfile.vue'
 
-  mounted() {
-    this.log_in_token = cookies.get(`log_in_token`);
-  },
-};
+   export default {
+  components: { DeleteProfile },
+        
+    }
+
 </script>
 
 <style scoped>
-.delete_div {
+div {
     display: grid;
     place-items: center;
 }
