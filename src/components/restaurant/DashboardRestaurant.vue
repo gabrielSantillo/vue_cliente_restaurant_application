3e2ecf7fd8896da60524d135f472b52bef8dedba
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2022-09-03 14:50:04 -0600

    Made the restaurant dashboard header looks better

--- src/components/restaurant/DashboardRestaurant.vue
+++ src/components/restaurant/DashboardRestaurant.vue
@@ -1,9 +1,15 @@
 <template>
   <div>
     <div class="header">
-      <h1>dashboard</h1>
-      <div>
+      <div class="row_one">
+        <router-link to="/restaurant-order">Orders</router-link>
+        <router-link to="/add-items">Add Items</router-link>
         <button @click="log_out_function">Log Out</button>
+      </div>
+      <div class="row_two">
+        <router-link to="/other-restaurants">Restaurants</router-link>
+        <router-link to="/profile-restaurant">Profile</router-link>
+        <router-link to="/delete-restaurant">Delete Profile</router-link>
       </div>
     </div>
 
@@ -17,11 +23,10 @@
 <script>
 import cookies from "vue-cookies";
 
-
 export default {
   data() {
     return {
-      log_in_token: null
+      log_in_token: null,
     };
   },
   mounted() {
@@ -46,7 +51,40 @@ export default {
 
 .header {
   display: grid;
-  grid-template-columns: 7fr 1fr;
-  place-items: center;
+  grid-template-rows: 1fr 1fr;
+  row-gap: 20px;
+  height: 100px;
+  margin-top: 20px;
+
+  > .row_one {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    place-items: center;
+
+    > button {
+      cursor: pointer;
+      margin-top: 10px;
+      border: none;
+      background: #ad1839;
+      color: white;
+      padding: 10px;
+      width: 80px;
+      border-radius: 5px;
+    }
+
+    > button:hover {
+      background: #bc2848;
+    }
+
+    > button:active {
+      transform: scale(0.95);
+    }
+  }
+
+  > .row_two {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    place-items: center;
+  }
 }
 </style>
--- src/views/restaurant/DashboardPageRestaurant.vue
+++ src/views/restaurant/DashboardPageRestaurant.vue
@@ -1,23 +1,10 @@
 <template>
   <div>
     <dashboard-restaurant></dashboard-restaurant>
-    <div class="dashboard_options">
-      <router-link to="/restaurant-order">Orders</router-link>
-      <router-link to="/add-items">Add Items</router-link>
-      <router-link to="/other-restaurants">Others Restaurants</router-link>
-      <router-link to="/profile-restaurant">Edit Profile</router-link>
-      <router-link to="/delete-restaurant">Delete Profile</router-link>
-    </div>
 
-    <section>
-      <div class="restaurant_profile">
-        <display-restaurant-info></display-restaurant-info>
-      </div>
+    <display-restaurant-info></display-restaurant-info>
 
-      <div class="restaurant_items">
-        <display-restaurant-items></display-restaurant-items>
-      </div>
-    </section>
+    <display-restaurant-items></display-restaurant-items>
   </div>
 </template>
 
@@ -34,17 +21,4 @@ export default {
 };
 </script>
 
-<style scoped>
-.dashboard_options {
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
-  place-items: center;
-  margin-top: 50px;
-}
-
-section {
-  margin-top: 100px;
-  place-items: center;
-  display: grid;
-}
-</style>
+<style scoped></style>
