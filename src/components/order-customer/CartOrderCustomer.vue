f7dbcb7c9957abc4155994d86875634bd08de50c
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2022-09-03 13:48:02 -0600

    Made the cart page looks better

--- src/components/order-customer/CartOrderCustomer.vue
+++ src/components/order-customer/CartOrderCustomer.vue
@@ -1,21 +1,24 @@
 <template>
   <div>
-      <h1>Your Cart</h1>
+    <div class="header">
       <router-link to="/restaurant-menu-options">Menu</router-link>
-    <section class="order_card" v-for="food in foods" :key="food[`id`]">
-      <div>
+      <div v-if="order_made === false">
+        <button @click="make_order(foods, $event)">Order</button>
+      </div>
+      <div v-else>
+        <button @click="order_status">Order Status</button>
+      </div>
+    </div>
+    <div class="line"></div>
+    <h1>Cart</h1>
+    <section class="cart">
+      <div v-for="food in foods" :key="food[`id`]">
         <img :src="food[`image_url`]" />
         <h3>{{ food[`name`] }}</h3>
         <h4>CAD$ {{ food[`price`] }}</h4>
         <button @click="delete_item(food, $event)">Delete</button>
       </div>
     </section>
-    <div v-if="order_made === false">
-      <button @click="make_order(foods, $event)">Order</button>
-    </div>
-    <div v-else>
-      <button @click="order_status">Order Status</button>
-    </div>
   </div>
 </template>
 
@@ -27,82 +30,135 @@ export default {
     return {
       foods: undefined,
       order_made: false,
-      orders_id: []
+      orders_id: [],
     };
   },
   mounted() {
     let foods_json = cookies.get(`cart`);
     this.foods = JSON.parse(foods_json);
 
-    if(cookies.get(`order_made`) !== null) {
-      this.order_made = true
+    if (cookies.get(`order_made`) !== null) {
+      this.order_made = true;
     }
   },
   methods: {
     delete_item(food) {
-      let cart = JSON.parse(cookies.get(`cart`))
-      for(let i = 0; i < cart.length; i++) {
-        if(food[`id`] === cart[i][`id`]){
-          cart.splice(i, 1)
-          cookies.set(`cart`, JSON.stringify(cart))
+      let cart = JSON.parse(cookies.get(`cart`));
+      for (let i = 0; i < cart.length; i++) {
+        if (food[`id`] === cart[i][`id`]) {
+          cart.splice(i, 1);
+          cookies.set(`cart`, JSON.stringify(cart));
           this.foods = cart;
-          return
+          return;
         }
       }
     },
     order_status() {
-      this.$router.push(`/order-status`)
+      this.$router.push(`/order-status`);
     },
     make_order() {
-      let menu_items_id = []
+      let menu_items_id = [];
       for (let i = 0; i < this.foods.length; i++) {
-        menu_items_id.push(this.foods[i][`id`])
+        menu_items_id.push(this.foods[i][`id`]);
       }
       axios
-          .request({
-            url: `https://innotechfoodie.ml/api/client-order`,
-            headers: {
-              "x-api-key": `RevyoqeHMCwaqRcUfmDC`,
-              token: `${cookies.get(`log_in_token`)}`,
-            },
-            method: `POST`,
-            data: {
-              menu_items: menu_items_id,
-              restaurant_id: cookies.get(`restaurant_id`),
-            },
-          })
-          .then((response) => {
-            this.orders_id.push(response[`data`][`order_id`])
-            let orders_id_json = JSON.stringify(this.orders_id)
-            cookies.set(`orders_id`, orders_id_json)
-            cookies.set(`order_made`, `yes`);
-            location.reload();
-          })
-          .catch((error) => {
-            error;
-          });
+        .request({
+          url: `https://innotechfoodie.ml/api/client-order`,
+          headers: {
+            "x-api-key": `RevyoqeHMCwaqRcUfmDC`,
+            token: `${cookies.get(`log_in_token`)}`,
+          },
+          method: `POST`,
+          data: {
+            menu_items: menu_items_id,
+            restaurant_id: cookies.get(`restaurant_id`),
+          },
+        })
+        .then((response) => {
+          this.orders_id.push(response[`data`][`order_id`]);
+          let orders_id_json = JSON.stringify(this.orders_id);
+          cookies.set(`orders_id`, orders_id_json);
+          cookies.set(`order_made`, `yes`);
+          location.reload();
+        })
+        .catch((error) => {
+          error;
+        });
     },
   },
 };
 </script>
 
 <style lang="scss" scoped>
-.order_card {
+.header {
   display: grid;
-  border-radius: 5px;
-  width: 100%;
   place-items: center;
-  margin-bottom: 25px;
+  grid-template-columns: 1fr 1fr;
+  margin-top: 20px;
+
+  > div {
+    > button {
+      cursor: pointer;
+      margin-top: 10px;
+      border: none;
+      background: #13542d;
+      color: white;
+      padding: 10px;
+      width: 120px;
+      border-radius: 5px;
+    }
+
+    > button:hover {
+      background: #196838;
+    }
+
+    > button:active {
+      transform: scale(0.95);
+    }
+  }
+}
+
+.line {
+  width: 90%;
+  height: 2px;
+  background-color: #13542d;
+  margin: 20px;
+}
+
+h1 {
+  text-align: center;
+}
+.cart {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+  place-items: center;
+  row-gap: 50px;
+  column-gap: 20px;
 
   > div {
     > img {
-      width: 300px;
-      height: 300px;
+      width: 250px;
+      height: 250px;
       object-fit: cover;
     }
 
-    > p {
-      width: 300px;
+    > button {
+      cursor: pointer;
+      margin-top: 10px;
+      border: none;
+      background: #ad1839;
+      color: white;
+      padding: 10px;
+      width: 80px;
+      border-radius: 5px;
+    }
+
+    > button:hover {
+      background: #bc2848;
+    }
+
+    > button:active {
+      transform: scale(0.95);
     }
   }
 }
