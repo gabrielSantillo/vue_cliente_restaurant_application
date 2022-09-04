c871b846314e78f32d2dc32da3da84696b986e5b
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2022-09-03 19:46:38 -0600

    Made the order page as restaurant looks better

--- src/components/order-restaurant/DisplayOrderRestaurant.vue
+++ src/components/order-restaurant/DisplayOrderRestaurant.vue
@@ -5,31 +5,42 @@
       <router-link to="dashboard-restaurant">Dashboard</router-link>
     </div>
 
+    <div class="red_line"></div>
+
     <div class="orders">
-      <h1>Not confirmed</h1>
-      <section v-for="order in is_not_confirmed" :key="order[`order_id`]">
+      <section
+        v-for="order in is_not_confirmed"
+        :key="order[`order_id`]"
+        class="orders_card red"
+      >
         <h3>Item: {{ order[`name`] }}</h3>
         <h4>CAD$ {{ order[`price`] }}</h4>
-        <div>
-          <button @click="confirm_order(order, $event)">Confirm</button>
-        </div>
+        <button @click="confirm_order(order, $event)">Confirm</button>
       </section>
     </div>
 
+    <div class="green_line"></div>
+
     <div class="orders">
-      <h1>Not Completed</h1>
-      <section v-for="order in is_not_completed" :key="order[`order_id`]">
+      <section
+        v-for="order in is_not_completed"
+        :key="order[`order_id`]"
+        class="orders_card green"
+      >
         <h3>Item: {{ order[`name`] }}</h3>
         <h4>CAD$ {{ order[`price`] }}</h4>
-        <div>
-          <button @click="complete_order(order, $event)">Complete</button>
-        </div>
+        <button @click="complete_order(order, $event)">Complete</button>
       </section>
     </div>
+
+    <div class="gray_line"></div>
 
     <div class="orders" v-if="orders.length >= 1">
-      <h1>Completed</h1>
-      <section v-for="order in is_completed" :key="order[`order_id`]">
+      <section
+        v-for="order in is_completed"
+        :key="order[`order_id`]"
+        class="orders_card gray"
+      >
         <h3>Item: {{ order[`name`] }}</h3>
         <h4>CAD$ {{ order[`price`] }}</h4>
       </section>
@@ -121,10 +132,12 @@ export default {
             this.orders[i][`is_complete`] === 0
           ) {
             this.is_not_completed.push(this.orders[i]);
-          } else if(this.orders[i][`is_confirmed`] === 1 &&
-            this.orders[i][`is_complete`] === 1) {
-              this.is_completed.push(this.orders[i])
-            }
+          } else if (
+            this.orders[i][`is_confirmed`] === 1 &&
+            this.orders[i][`is_complete`] === 1
+          ) {
+            this.is_completed.push(this.orders[i]);
+          }
         }
       })
       .catch((error) => {
@@ -144,13 +157,69 @@ export default {
 
 .orders {
   display: grid;
-  grid-auto-flow: column;
   place-items: center;
-  column-gap: 10px;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  column-gap: 100px;
 
   > section {
-    box-shadow: 8px 8px 16px rgb(188, 188, 188);
-    padding: 5px;
+    > button {
+      cursor: pointer;
+      margin-top: 10px;
+      border: none;
+      background: #13542d;
+      color: white;
+      padding: 10px;
+      width: 100px;
+      border-radius: 5px;
+    }
+
+    > button:hover {
+      background: #196838;
+    }
+
+    > button:active {
+      transform: scale(0.95);
+    }
   }
+}
+
+.orders_card {
+  display: grid;
+  place-items: center;
+  border-radius: 5px;
+  box-shadow: 7px 7px 13px 0px rgba(50, 50, 50, 0.22);
+  padding: 10px;
+  width: 280px;
+  margin: 20px;
+}
+
+.red {
+  border-left: 3px solid #ad1839;
+}
+
+.green {
+  border-left: 3px solid #409c1f;
+}
+
+.gray {
+  border-left: 3px solid gray;
+}
+
+.red_line {
+  width: 100%;
+  background: #ad1839;
+  height: 2px;
+}
+
+.green_line {
+  width: 100%;
+  background: #409c1f;
+  height: 2px;
+}
+
+.gray_line {
+  width: 100%;
+  background: gray;
+  height: 2px;
 }
 </style>
